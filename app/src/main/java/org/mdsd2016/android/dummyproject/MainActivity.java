aac43c35a933b26c312a3fa02377a1e111d3e80e
Vinicius <vinicius.scheidegger@gmail.com>
2017-01-23 11:58:48 +0100

    Added Activity Main

---
+++ app/src/main/java/org/mdsd2016/android/dummyproject/MainActivity.java
@@ -0,0 +1,16 @@
+package org.mdsd2016.android.dummyproject;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Vini on 1/23/2017.
+ */
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
